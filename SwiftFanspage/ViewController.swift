4ec71f6b9f6e311a7b7f5082759294efc4a4c207
One <appdevoney@gmail.com>
2014-06-04 23:58:45 +0800

    Initial Commit

---
+++ SwiftFanspage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftFanspage
+//
+//  Created by SpoonRocket on 2014/6/4.
+//  Copyright (c) 2014年 oney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
