2c7d7e4080e310c3af4c7e6fd900615a60f42e91
One <appdevoney@gmail.com>
2014-06-05 03:37:32 +0800

    add API class

--- SwiftFanspage/WebImageOperations.swift
+++ SwiftFanspage/WebImageOperations.swift
@@ -12,7 +12,7 @@ class WebImageOperations: NSObject {
     class func processImageDataWithURLString(urlString :NSString, handler:((NSData!) -> Void)!) {
         var url :NSURL = NSURL.URLWithString(urlString)
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
-            var imageData:NSData = NSData.dataWithContentsOfURL(url, options: NSDataReadingOptions.DataReadingMappedIfSafe, error: nil)
+            var imageData:NSData = NSData.dataWithContentsOfURL(url, options: .DataReadingMappedIfSafe, error: nil)
             NSOperationQueue.mainQueue().addOperationWithBlock {
                 handler(imageData)
             }
