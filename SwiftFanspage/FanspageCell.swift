271f88327ece59ae1c4555667654868df06702c1
Howard Yang <howard@spoonrocket.com>
2014-06-05 00:05:01 +0800

    First

---
+++ SwiftFanspage/FanspageCell.swift
@@ -0,0 +1,33 @@
+//
+//  FanspageCell.swift
+//  Xcode6test
+//
+//  Created by SpoonRocket on 2014/6/4.
+//  Copyright (c) 2014年 spoonrocket. All rights reserved.
+//
+
+import UIKit
+
+class FanspageCell: UITableViewCell {
+
+    @IBOutlet var name :UILabel
+    @IBOutlet var category :UILabel
+    @IBOutlet var photo :UIImageView
+    
+    init(style: UITableViewCellStyle, reuseIdentifier: String) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        // Initialization code
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
